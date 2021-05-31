31972ae264293236765f1f4db42655a7f61a67fd
Davi <davihugomp@gmail.com>
2021-05-31 00:28:04 -0300

    Created base models

---
+++ app/mod_common/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class BaseSimple(db.Model):
+  __abstract__ = True
+
+  id = db.Column(db.Integer, primary_key=True)
+
+class Base(db.Model):
+  __abstract__ = True
+
+  id            = db.Column(db.Integer, primary_key=True)
+  creationDate  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+  lastUpdate    = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                                           onupdate=db.func.current_timestamp())
